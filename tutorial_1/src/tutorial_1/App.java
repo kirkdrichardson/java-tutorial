09dd7410e930104546be2b1216a8d0da07ef09b0
kirk <kirk.douglas.richardson@gmail.com>
2018-01-06 21:05:42 -0700

    primitive types

--- tutorial_1/src/tutorial_1/App.java
+++ tutorial_1/src/tutorial_1/App.java
@@ -3,7 +3,18 @@ package tutorial_1;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.print("Hello World");
+		
+		// the 8 primitive types
+		int myInt = 456;
+		short myShort = 45;
+		long myLong = 999999999;
+		double myDouble = 9.999999;
+		float myFloat = 9.99f;
+		char myChar = 'h';
+		boolean myBool = true;
+		byte myByte = 127;
+		
+		System.out.print(myChar);
 	}
 
 }
